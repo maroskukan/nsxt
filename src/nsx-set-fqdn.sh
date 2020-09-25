78bbac6d97b71b8f4840800dd3f25be3c28ad84f
Maros Kukan <maros.kukan@me.com>
2020-09-25 14:15:29 +0200

    Functions added

--- src/nsx-set-fqdn.sh
+++ src/nsx-set-fqdn.sh
@@ -3,38 +3,70 @@
 # Purpose: Enables FQDN usage on NSX Manager
 # Author: Maros Kukan
 
-declare nsx_user
-declare nsx_pass
-declare nsx_auth
-declare -l nsx_fqdn
 declare -l nsx_path
 
-#
-# Verify if NSX FQDN variable is available globally
-# (e.g declare -xl nsx_fqdn=nsxmgr.example.com)
-#
-if [[ -z $nsx_fqdn ]]; then
-    echo -n "Enter the NSX-T Manager FQDN: "
-    declare -l nsx_fqdn
-    read nsx_fqdn
-else
-    echo "NSX-T Manager FQDN loaded from environment"
-fi
-#
-# Collect credentials
-#
-while [[ -z $nsx_user ]]
-do
-    read -r -p "Enter the NSX-T Username: " nsx_user
-done
+function get_fqdn() {
+    # Verify if NSX FQDN variable is available globally
+    # (e.g declare -xl nsx_fqdn=nsxmgr.example.com)
+    if [[ -z $nsx_fqdn ]]; then
+        while [[ -z $nsx_fqdn ]]
+        do
+            read -r -p "Enter the NSX-T Manager FQDN: " nsx_fqdn
+        done
+    else
+        echo "NSX-T Manager FQDN loaded from environment."
+    fi
+}
 
-while [[ -z $nsx_pass ]]
-do
-    read -r -s -p "Enter the NSX-T Password: " nsx_pass
-    echo 
-done
 
-#
-# Convert credentials to Base64
-#
-nsx_auth=$(echo -n ${nsx_user}:${nsx_pass} | base64)
+function get_creds () {
+    # Collect credentials and return Base64 string
+    if [[ -z $nsx_user ]]; then
+        while [[ -z $nsx_user ]]
+        do
+            read -r -p "Enter the NSX-T Username: " nsx_user
+        done
+    else
+        echo "NSX-T Username loaded from environment."
+    fi
+    if [[ -z $nsx_pass ]]; then
+        while [[ -z $nsx_pass ]]
+        do
+            read -r -s -p "Enter the NSX-T Password: " nsx_pass
+            echo 
+        done
+    else
+        echo "NSX-T Password loaded from environment."
+    fi
+    nsx_auth=$(echo -n ${nsx_user}:${nsx_pass} | base64)
+}
+
+
+function check_api() {
+    # Verify if API Service and Credentials are valid
+    local fqdn=$1
+    local auth=$2
+    local response=$(curl "https://${fqdn}/api/v1/cluster/api-service" \
+    --insecure \
+    --silent \
+    --head \
+    --header "Authorization: Basic $auth")
+    echo $response
+    local status_code=$(echo $response | sed -n 's/HTTP.* \(.*\) .*/\1/p')
+    if [[ ! $status_code == 200 ]]; then
+         echo "API Service is not available. Status code is $status_code"
+         exit 1
+    else
+         echo "API Service is available"
+    fi
+}
+
+
+
+
+get_fqdn
+get_creds
+echo $nsx_fqdn
+echo $nsx_auth
+
+check_api $nsx_fqdn $nsx_auth
